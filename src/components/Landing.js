058fa15f04b5c6985b632d46cc24e60cf61b5532
Shubham Chinda <shubhamchinda07@gmail.com>
2018-08-29 00:22:10 +0530

    UI footer

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -36,7 +36,7 @@ export default class LandingPage extends Component{
           <Panel.Heading>
             <h4 className="text-center">USER 2</h4>
           </Panel.Heading>
-            <Panel.Body>
+            <Panel.Body className="boddy">
               <Image src="https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg" className="random img-thumbnail" />
             </Panel.Body>
               <ListGroup>
@@ -76,37 +76,38 @@ export default class LandingPage extends Component{
 
       </Col>
 
-    </Row>
+    </Row><br/>
   </Grid>
 
     <div className="footer">
       <footer>
       <div className="container">
         <Row>
-          <Col xs={1} md={1}>
+          <Col xs={3} md={1}>
             <Image src="https://image.ibb.co/gBdw7p/Screenshot_224.png" width="80" height="50" className="fimage" />
             </Col>
-          <Col xs={3} md={3} className="filmy">
+          <Col md={3} className="filmy hidee">
             <h4><b>filmyprofile.com - 2018</b></h4>
           </Col>
-          <Col xs={4} md={4}>
+          <Col md={4} className="hidee">
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={2} md={1}>
             <h4><b>FAQs</b></h4>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={2} md={1}>
             <h4><b>Contact Us</b></h4>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={2} md={1}>
             <h4><b>About Us</b></h4>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={2} md={1}>
             <h4><b>Terms</b></h4>
           </Col>
         </Row>
       </div>
       </footer>
     </div>
+
 
   </div>
 
